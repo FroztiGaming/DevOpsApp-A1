4b4361a29e18f65cefb45c9622e0388b26aa0eec
Daniel <daniel.hawkes002@student.weltec.ac.nz>
2021-04-22 17:40:01 +1200

    Added create endpoint

--- app.py
+++ app.py
@@ -3,6 +3,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	return 'Hello, World!'
+    return 'Hello, World!'
+
+NEXT_ID = 0
+TODOS = {}
+
+@app.route('/create/<name>')
+def create(name):
+    global NEXT_ID
+    TODOS[NEXT_ID] = {'name': name, 'done': False}
+    NEXT_ID += 1
+    return TODOS
 
 print("Nick was here 2021")
+print(" Dan was here 2021")
