0642778f76e53ef9a34f6019b35448cca533db14
Daniel <daniel.hawkes002@student.weltec.ac.nz>
2021-04-22 17:42:46 +1200

    Fixed merge mistake

--- app.py
+++ app.py
@@ -3,9 +3,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-<<<<<<< HEAD
-	return 'Hello, World!\nNick was here 2021'
-=======
     return 'Hello, World!'
 
 NEXT_ID = 0
@@ -17,7 +14,6 @@ def create(name):
     TODOS[NEXT_ID] = {'name': name, 'done': False}
     NEXT_ID += 1
     return TODOS
->>>>>>> dan
 
 print("Nick was here 2021")
 print(" Dan was here 2021")
